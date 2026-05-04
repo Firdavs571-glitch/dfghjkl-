7373c89ac32ca3996179299305333a369beae724
Firdavs <student1846@tuit.uz>
2026-05-04 15:34:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -278,7 +278,7 @@ bot.on("message", async (msg) => {
         `Username: @${msg.from.username || "yo‘q"}`
       );
 
-      return bot.sendMessage(chatId, "✅ Adminga xabar yuborildi.");
+      return bot.sendMessage(chatId, "✅ Adminga xabar yuborildi.yoki o'ziz yozing @TUIT_SESSIYA111");
     }
 
     if (text === "🔓 Blokdan ochish" && user.isAdmin) {
